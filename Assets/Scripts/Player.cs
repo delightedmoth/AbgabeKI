7bc2ae05e328b8a166b25f563bb0447151516361
delightedmoth <11016864@STUD.HOCHSCHULE-HEIDELBERG.DE>
2023-12-17 11:26:08 +0100

    new jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,40 +1,84 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private CharacterController character;
-    private Vector3 direction;
+    private Rigidbody2D rb;
+    private Vector2 direction;
 
+    public float jumpTime;
     public float gravity = 9.81f * 2f;
     public float jumpForce = 8f;
+    public float jumpMultiplier = 4f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Vector2 vecGravity;
+
+    private bool isJumping;
+    private float jumpCounter;
 
     private void Awake()
     {
-        character = GetComponent<CharacterController>();
+        vecGravity = new Vector2(0, -Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
-        direction = Vector3.zero;
+        direction = Vector2.zero;
     }
 
     private void Update()
     {
-        direction += Vector3.down * gravity * Time.deltaTime;
-
-        if (character.isGrounded)
+        if (Input.GetButton("Jump") && isGrounded()) 
         {
-            direction = Vector3.down;
+            rb.velocity = new Vector2(0, jumpForce);
+            isJumping = true;
+            jumpCounter = 0;
+        }
 
-            if (Input.GetButton("Jump"))
+        if (rb.velocity.y > 0 && isJumping)
+        {
+            jumpCounter += Time.deltaTime;
+            if (jumpCounter > jumpTime) isJumping = false;
+
+            float t = jumpCounter / jumpTime;
+            float currentJumpM = jumpMultiplier;
+
+            if (t > 0.5f)
             {
-                direction = Vector3.up * jumpForce;
+                currentJumpM = jumpMultiplier * (1 - t);
+            }
+
+            rb.velocity += vecGravity * currentJumpM * Time.deltaTime;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            jumpCounter = 0;
+
+            if (rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y * 0.6f);
             }
         }
-        
-        character.Move(direction * Time.deltaTime);
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity -= vecGravity * gravity * Time.deltaTime;
+        }
+
     }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position,
+            new Vector2(0.5f, 1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+    
 }
