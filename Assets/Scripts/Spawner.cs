bbc6a64d0f1946da0afa089a328d64f5202da449
delightedmoth <11016864@STUD.HOCHSCHULE-HEIDELBERG.DE>
2023-12-17 13:39:56 +0100

    Spawner finished

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
@@ -11,5 +14,37 @@ public class Spawner : MonoBehaviour
         [Range(0f, 1f)] public float spawnChance;
     }
 
-    public SpawnableObject[] obstacles;
+    [FormerlySerializedAs("obstacles")] public SpawnableObject[] objects;
+
+    public float minSpawnRate = 1f;
+    public float maxSpawnRate = 2f;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn()
+    {
+        float spawnChance = Random.value;
+
+        foreach (var obj in objects)
+        {
+            if (spawnChance < obj.spawnChance)
+            {
+                GameObject obstacle = Instantiate(obj.prefab);
+                obstacle.transform.position += transform.position;
+                break;
+            }
+
+            spawnChance -= obj.spawnChance;
+        }
+        
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
 }
